fdcb773dedc8f953e04949672d949bd03d17fd63
Bengie Nick <bengiexd@gmail.com>
2013-11-10 23:03:37 -0500

    test

---
+++ src/libCapaPresentacion/Main.java
@@ -0,0 +1,16 @@
+package libCapaPresentacion;
+
+import java.util.ArrayList;
+
+import libCapaConexion.CConexion;
+import libCapaLogica.CEntidad;
+
+public class Main {
+	public static void main(String nick[]){				
+		frmUsuario u = new frmUsuario();
+		u.show();
+		
+		//frmCliente c = new frmCliente();
+		//c.show();
+	}
+}
